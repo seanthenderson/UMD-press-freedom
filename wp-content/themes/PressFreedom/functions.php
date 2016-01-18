db5253d87034bfb4192089a85cd2a889dbf2a259
seanthenderson <seanthenderson@gmail.com>
2016-01-18 14:19:24 -0500

    Convert sidebar template to PHP

--- wp-content/themes/PressFreedom/functions.php
+++ wp-content/themes/PressFreedom/functions.php
@@ -2,6 +2,12 @@
 	// Hide admin bar
 	show_admin_bar(true);
 
+	// Turn on debugging
+	define( 'WP_DEBUG', true );
+
+	// Flush cache
+	wp_cache_flush();	
+
 	// Tell WP Super Cache & W3 Total Cache to not cache WPReadable requests
 	define( 'DONOTCACHEPAGE', true );
 
@@ -25,7 +31,7 @@
 	// Imprisoned journalist custom content 
 	function imprisoned_journalist() {
 	  $args = array();
-	  register_post_type( 'product', $args ); 
+	  register_post_type( 'journalists', $args ); 
 	}
 	add_action( 'init', 'imprisoned_journalist' );
 
@@ -53,7 +59,35 @@
 	    'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
 	    'has_archive'   => true,
 	  );
-	  register_post_type( 'product', $args ); 
+	  register_post_type( 'journalists', $args ); 
+	  flush_rewrite_rules();
 	}
 	add_action( 'init', 'imprisoned_journalist_options' );
+
+	// Number of days journalist has been imprisoned
+	function days_in_jail() {
+		$now = time();
+	    $date = get_field("date_imprisoned");
+	    $date = join('-', str_split($date, 4));
+	    $date = join('-', str_split($date, 7));
+	    $date = strtotime($date);
+	    $datediff = $now - $date;
+	    $datediff = floor($datediff/(60*60*24));
+	    $datediff = number_format($datediff);
+	    echo $datediff; 
+	}
+
+	// Add support links to admin menu
+	add_action('admin_bar_menu', 'support_links', 999);
+
+	function support_links($wp_admin_bar) {
+		$args = array(
+			'id'    => 'support_links',
+			'title' => 'Support Links',
+			'href'  => 'Press-Freedom/wp-admin/post.php?post=30&action=edit',
+			'meta'  => array('class' => 'support-links')
+		);
+		$wp_admin_bar->add_node($args);
+	}
+
 ?>
--- wp-content/themes/PressFreedom/sidebar.php
+++ wp-content/themes/PressFreedom/sidebar.php
@@ -1,30 +1,34 @@
 <div class="right-column">
-	<div class="imprisoned-journalists-box">
-		<h4>Imprisoned Journalist Name</h4>
-		<div class="headshot">headshot</div>
-		<p>Vim euripidis reformidans ut. Ut has prompta temporibus ullamcorper, ne his rebum novum iisque, stet zril facilis mei ex. Primis vidisse tractatos an usu, pro et mundi aperiri malorum. In nihil neglegentur accommodare sea, no dicit omittam usu. Vim euripidis reformidans ut. Ut has prompta temporibus ullamcorper, ne his rebum novum iisque, stet zril facilis mei ex.</p>
-		<div class="days-jailed">
-			Days in jail: <span>317</span>
-		</div>
-	</div>
-	<div class="imprisoned-journalists-box">
-		<h4>Imprisoned Journalist Name</h4>
-		<div class="headshot">headshot</div>
-		<p>Vim euripidis reformidans ut. Ut has prompta temporibus ullamcorper, ne his rebum novum iisque, stet zril facilis mei ex. Primis vidisse tractatos an usu, pro et mundi aperiri malorum. In nihil neglegentur accommodare sea, no dicit omittam usu. Vim euripidis reformidans ut. Ut has prompta temporibus ullamcorper, ne his rebum novum iisque, stet zril facilis mei ex.</p>
-		<div class="days-jailed">
-			Days in jail: <span>690</span>
-		</div>
-	</div>
+	<?php 
+		$args = array(
+			'post_type' => 'journalists',
+			'posts_per_page' => '2'
+		);
+		$sidebar = new WP_Query($args);
+		if ($sidebar->have_posts()) {
+			while ($sidebar->have_posts()) {
+				$sidebar->the_post(); ?>
+			    <a href="<?php the_permalink(); ?>">
+			    	<div class="imprisoned-journalists-box">
+					    <h4><?php the_title(); ?></h4>
+					    <div class="headshot">
+			    	 		<?php if ( has_post_thumbnail() ) { 
+			    	 			the_post_thumbnail();
+			    		 	} ?>
+					    </div>
+					    <p><?php the_excerpt(); ?></p>
+					    <div class="days-jailed">
+					    	Days in jail: <span><?php echo days_in_jail(); ?></span>
+					    </div>
+					</div>
+			    </a>
+			<?php } 
+		} 
+	?>
 	<div class="ad sidebar-ad">300 x 250 AD</div>
-	<div class="related-links">
+	<div class="categories-list">
 		<h3>Key Categories</h3>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>		
-		<div class="editors-pick"><a href="">Category name goes here</a></div>
-		<div class="editors-pick"><a href="">Category name goes here</a></div>						
+		<?php wp_list_categories(); ?>					
 	</div>
 	<div class="related-links">
 		<h3>Links to Related Sites</h3>
