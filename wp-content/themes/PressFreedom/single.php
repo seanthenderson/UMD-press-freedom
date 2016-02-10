1a1c82dc5508873dfe96278d58a68b8f9fd69b7f
seanthenderson <seanthenderson@gmail.com>
2016-02-09 20:30:28 -0500

    Move byline box into seperate file

--- wp-content/themes/PressFreedom/single.php
+++ wp-content/themes/PressFreedom/single.php
@@ -17,25 +17,7 @@
 			</div>
 		 </div>
 
-		<div class="byline-box">
-			<?php foreach( get_coauthors() as $coauthor ) : ?>
-				<h3>
-					<?php if(function_exists('coauthors_posts_links')) { ?>
-						<?php coauthors_posts_links(); 
-					} else { ?>
-						By <?php get_the_author();
-					} ?>
-				</h3>
-				<div class="headshot">
-					<a href="<?php echo $coauthor->user_url; ?>"><?php echo get_avatar('small'); ?></a>
-					<i class="fa fa-twitter"></i>
-					<i class="fa fa-linkedin"></i>
-				</div>
-				<p>
-					<?php echo $coauthor->description; ?>
-				</p>
-			<?php endforeach; ?>
-		</div>
+		<?php include('includes/byline-box.php'); ?>
 
 		<div class="social-share">
 			<i class="fa fa-facebook"></i>
@@ -53,5 +35,5 @@
 	<div class="right-column">
 		<?php get_sidebar(); ?>
 	</div>
-	
+
 <?php get_footer(); ?>
