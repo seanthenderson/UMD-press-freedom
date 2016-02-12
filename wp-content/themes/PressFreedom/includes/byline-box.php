0e2aeb032addb6f112b418bf7cd6d3395f7b7742
seanthenderson <seanthenderson@gmail.com>
2016-02-11 21:38:37 -0500

    Hook up social media icons in byline box

--- wp-content/themes/PressFreedom/includes/byline-box.php
+++ wp-content/themes/PressFreedom/includes/byline-box.php
@@ -8,9 +8,18 @@
 			} ?>
 		</h3>
 		<div class="headshot">
+			<?php $author_id = get_the_author_meta('ID'); ?>
 			<a href="<?php echo $coauthor->user_url; ?>"><?php echo get_avatar('small'); ?></a>
-			<i class="fa fa-twitter"></i>
-			<i class="fa fa-linkedin"></i>
+			<div class="social-icon"> 	
+				<a href="<?php the_field('twitter', 'user_'. $author_id); ?>" target="_blank">
+					<i class="fa fa-twitter"></i>
+				</a>
+			</div>
+			<div class="social-icon">
+				<a href="<?php the_field('linkedin', 'user_'. $author_id); ?>" target="_blank" class="social-icon">
+					<i class="fa fa-linkedin"></i>
+				</a>
+			</div>
 		</div>
 		<p>
 			<?php echo $coauthor->description; ?>
