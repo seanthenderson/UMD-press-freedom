08483d8a0f55bc32f24dc20908cb8b62ff665456
seanthenderson <seanthenderson@gmail.com>
2016-02-14 13:19:47 -0500

    Add script files to footer

--- wp-content/themes/PressFreedom/footer.php
+++ wp-content/themes/PressFreedom/footer.php
@@ -2,6 +2,8 @@
 			<footer>
 				<div class="copyright">&copy; Philip Merrill College of Journalism</div>
 				<?php wp_footer(); ?>
+				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+				<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js" type="text/javascript"></script>
 			</footer>
 		</div>
 	</body>
