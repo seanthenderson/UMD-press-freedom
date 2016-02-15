24bde13ec4157e5fa182b03ecbc8d5ce19ee3164
seanthenderson <seanthenderson@gmail.com>
2016-02-14 22:08:49 -0500

    Add Facebook JS code

--- wp-content/themes/PressFreedom/js/script.js
+++ wp-content/themes/PressFreedom/js/script.js
@@ -12,3 +12,18 @@ $(document).ready(function() {
 		}
 	});
 });
+// Facebook share button
+window.fbAsyncInit = function(){
+FB.init({
+    appId: 'xxxxx', status: true, cookie: true, xfbml: true }); 
+};
+(function(d, debug){var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if(d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; 
+    js.async = true;js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+    ref.parentNode.insertBefore(js, ref);}(document, /*debug*/ false));
+function postToFeed(title, desc, url, image){
+var obj = {method: 'feed',link: url, picture: 'http://www.url.com/images/'+image,name: title,description: desc};
+function callback(response){}
+FB.ui(obj, callback);
+}
