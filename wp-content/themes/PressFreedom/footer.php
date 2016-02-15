21cb792d1355e68dd459adf8a6319bef022ecb3b
seanthenderson <seanthenderson@gmail.com>
2016-02-14 22:07:00 -0500

    Add Facebook code to footer

--- wp-content/themes/PressFreedom/footer.php
+++ wp-content/themes/PressFreedom/footer.php
@@ -4,6 +4,18 @@
 				<?php wp_footer(); ?>
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 				<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/script.js" type="text/javascript"></script>
+				<!--===== FACEBOOK CODE =====-->
+				<!-- Load Facebook SDK for JavaScript -->
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+				<!--===== TWITTER CODE =====-->
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 			</footer>
 		</div>
 	</body>
