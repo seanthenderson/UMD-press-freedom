0a72cef68024f53926a0fd67e8b6c4197ca1ab8f
seanthenderson <seanthenderson@gmail.com>
2016-02-28 20:32:30 -0500

    Add released journalists section to journalists hub page

--- wp-content/themes/PressFreedom/archive-journalists.php
+++ wp-content/themes/PressFreedom/archive-journalists.php
@@ -29,7 +29,7 @@
 
 		    var map = new google.maps.Map(document.getElementById('full-map'), {
 		      zoom: 2,
-		      center: new google.maps.LatLng(33, 10),
+		      center: new google.maps.LatLng(33, 8),
 		      mapTypeId: google.maps.MapTypeId.ROADMAP
 		    });
 
@@ -55,7 +55,14 @@
 		<div class="journalist-cards">
 			<?php 
 			 	$args = array(
-			 		'post_type' => 'journalists'
+			 		'post_type' => 'journalists',
+			 		'meta_query' => array(
+			 			array(
+			 				'key' => 'released',
+			 				'value' => '0',
+			 				'compare' => '=='
+			 			)
+			 		)
 			 	);
 			 	$category = new WP_Query($args);
 			 	if ($category->have_posts()) {
@@ -68,7 +75,7 @@
 				    				$url = $image_array[0]; ?>
 									<div class="headshot" style="background: url('<?php echo $url; ?>') no-repeat center center; background-size: cover;"></div> 
 								<?php } ?>
-								<div class="corner-days"><?php days_in_jail(); ?></div>
+								<div class="corner-days"><?php days_in_jail(); ?> days</div>
 								<div class="info">	
 									<h2 class="name"><?php the_title(); ?></h2>
 									<h3 class="country"><?php the_field("country"); ?></h3>
@@ -88,11 +95,60 @@
 								</a>
 							</div>
 						</div>
-							
 			 		<?php } 
 			 	} 
 			?>
 		</div>
+
+		<h2 class="released">Released</h2>
+		<div class="journalist-cards">
+			<?php 
+			 	$args = array(
+			 		'post_type' => 'journalists',
+			 		'meta_query' => array(
+			 			array(
+			 				'key' => 'released',
+			 				'value' => '1',
+			 				'compare' => '=='
+			 			)
+			 		)
+			 	);
+			 	$category = new WP_Query($args);
+			 	if ($category->have_posts()) {
+			 		while ($category->have_posts()) {
+			 			$category->the_post(); ?>
+			 			<div class="journalist-card">
+							<a href="<?php the_permalink(); ?>">	
+						 		<?php if (has_post_thumbnail()) { 
+				    				$image_array = wp_get_attachment_image_src(\get_post_thumbnail_id( $page_id ), 'optional-size' );
+				    				$url = $image_array[0]; ?>
+									<div class="headshot" style="background: url('<?php echo $url; ?>') no-repeat center center; background-size: cover;"></div> 
+								<?php } ?>
+								<div class="released">Released</div>
+								<div class="info">	
+									<h2 class="name"><?php the_title(); ?></h2>
+									<h3 class="country"><?php the_field("country"); ?></h3>
+									<div class="days-jailed">
+										<?php the_title(); ?> was in prison for
+										<span><?php the_field("days_spent_in_jail"); ?></span> days
+										in <?php the_field("country_where_imprisoned"); ?> for the crime of being a journalist
+									</div>
+								</div>
+							</a>
+							<div class="social-media">	
+								<a href="">
+									<i class="fa fa-facebook"></i>
+								</a>
+								<a href="https://twitter.com/share?text=<?php the_title(); ?>" target="_blank" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+									<i class="fa fa-twitter"></i>
+								</a>
+							</div>
+						</div>
+			 		<?php } 
+			 	} 
+			?>
+		</div>
+
 		<section class="press-uncuffed">
 		 	<?php $pressUncuffed = new WP_Query('pagename=press-uncuffed');
 		 		while ($pressUncuffed->have_posts()) :
@@ -127,7 +183,7 @@
 								    <div class="headshot" style="background: url('<?php echo $url; ?>') no-repeat center center; background-size: cover;">
 						    		 	<h3>
 						    		 		<?php the_title(); ?>
-						    		 		<p><?php echo get_excerpt(230); ?></p>
+						    		 		<p><?php echo get_excerpt(200); ?></p>
 						    		 	</h3>
 								    </div>	    					
 			    				<?php }
