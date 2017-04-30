17b8fba3b6df8d4b0678a79cfd13ef9df8eaad85
seanthenderson <seanthenderson@gmail.com>
2017-04-30 17:40:16 -0400

    Fix for articles with multiple authors

--- wp-content/themes/PressFreedom/includes/byline-box.php
+++ wp-content/themes/PressFreedom/includes/byline-box.php
@@ -1,12 +1,15 @@
 <div class="byline-box">
-	<?php foreach( get_coauthors() as $coauthor ) : ?>
-		<h3>
-			<?php if(function_exists('coauthors_posts_links')) { ?>
-				<?php coauthors_posts_links(); 
-			} else { ?>
-				By <?php get_the_author();
-			} ?>
-		</h3>
+	<?php $x = 1; 
+	foreach( get_coauthors() as $coauthor ) : 
+		if ($x == 1) { ?>
+			<h3>
+				<?php if(function_exists('coauthors_posts_links')) { ?>
+					<?php coauthors_posts_links(); 
+				} else { ?>
+					By <?php get_the_author();
+				} ?>
+			</h3>
+		<?php } ?>
 		<div class="headshot">
 			<?php $author_id = get_the_author_meta('ID'); ?>
 			<?php echo get_avatar($coauthor->user_email, 'small'); ?>
@@ -24,5 +27,6 @@
 		<p>
 			<?php echo $coauthor->description; ?>
 		</p>
-	<?php endforeach; ?>
+		<?php $x++; 
+	endforeach; ?>
 </div>
